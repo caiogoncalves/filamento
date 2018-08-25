e43abd4a5f271faadd2857f037b0301e0a1b77a9
caioagoncalves@gmail.com <cag2210M@ri>
2018-08-24 23:22:16 -0300

    Criado o formulario em html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -4,17 +4,20 @@ from app.models.form import EmailForm
 from flask_mail import  Message
 
 
+
 @app.route('/', methods=['POST', 'GET'])
 
 def hello_world():
     form = EmailForm()
     if form.validate_on_submit():
 
+
         msg = Message(
             "Novo Contato",
             sender='caioagoncalves@gmail.com',
             recipients=['caioagoncalves@gmail.com'],
-            body="E-Mail: %s, Nome: %s, Telefone: %s, Assunto: %s" %(form.email.data, form.nome.data, form.telefone.data, form.mensagem.data)
+            body="E-Mail: %s, Nome: %s, Telefone: %s, Assunto: %s" %(form.email.data, form.nome.data, form.telefone.data, form.mensagem.data),
+            html = render_template('email.html', form = form)
         )
         mail.send(msg)
     else:
